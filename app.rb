d6602d6dd554c0a6003cde890ef21d504d4d62fc
luckypool <luckypool314@gmail.com>
2018-09-11 22:20:31 +0900

    Add sample api

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
+require 'grape'
 
-class App < Sinatra::Base
+class API < Grape::API
+  content_type :json, 'application/json'
+  default_format :json
+
+  get :hello do
+    { hello: 'world' }
+  end
+end
+
+class Web < Sinatra::Base
   get '/' do
     'Hello world!'
   end
